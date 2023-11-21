ea7c965f51cc6a72af17a91a7c4d62e8da7bef4d
myusername <syahrulbelfort@gmail.com>
2023-11-21 18:12:24 +0700

    [update] - page

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div role="alert" className="alert alert-success">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="stroke-current shrink-0 h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-        />
-      </svg>
-      <span>Your purchase has been confirmed!</span>
+    <div>
+      <h2 className="font-jakarta font-bold text-3xl">Relax Your Nature</h2>
     </div>
   );
 }
