db1ad6f0c4b11ca7d60ab16a473d3e30ba4d20e5
myusername <syahrulbelfort@gmail.com>
2023-11-21 18:00:40 +0700

    [Add] - Color Kits and font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,14 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      primary: "#E48400",
+      textAccent: "#7A7774",
+      text: "#191003",
+    },
+    fontFamily: {
+      sans: ["Plus Jakarta Sans", "Satoshi", "sans-serif", "Inter"],
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
