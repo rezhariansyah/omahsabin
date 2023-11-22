e12ef9443fc6ea5307ac40244711375e0dd81cfb
myusername <syahrulbelfort@gmail.com>
2023-11-22 10:51:50 +0700

    [Add] - content2

---
+++ app/dev/page.tsx
@@ -0,0 +1,13 @@
+// pages/app/dashboard.js
+import Content2 from "@/components/Content2";
+import React from "react";
+
+const DevPage = () => {
+  return (
+    <div>
+      <Content2 />
+    </div>
+  );
+};
+
+export default DevPage;
