95f8cc75127d1b6a19fd5b343349461516dd655e
myusername <syahrulbelfort@gmail.com>
2023-11-25 14:44:27 +0700

    update: maxwidth

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const Layout = ({ children }: Props) => {
   return (
-    <div className="max-w-7xl mx-auto px-4 md:px-8 min-h-screen">
+    <div className="max-w-[1440px] mx-auto px-4 md:px-8 min-h-screen">
       <div className="lg:hidden">
         <MobileViewNavbar>{children}</MobileViewNavbar>
       </div>
