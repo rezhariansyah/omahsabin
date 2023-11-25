cbd3edc2cda390ef1e67b4c247363dd75de7982c
myusername <syahrulbelfort@gmail.com>
2023-11-25 14:17:26 +0700

    feat: slider content 3

--- components/Content2/Content2.tsx
+++ components/Content2/Content2.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import React from "react";
 import contentImage1 from "../../public/assets/images/content2img1.png";
 import contentImage2 from "../../public/assets/images/content2img2.png";
@@ -6,8 +6,28 @@ import contentImage3 from "../../public/assets/images/content2img3.png";
 import Modals from "../modals";
 
 export default function Content2() {
+  interface ImageItemProps {
+    image: StaticImageData;
+    alt: string;
+  }
+  const ImageItem: React.FC<ImageItemProps> = ({ image, alt }) => (
+    <div className="w-full md:w-auto">
+      <Modals image={image} alt={alt} />
+    </div>
+  );
+
+  interface ImageInfo {
+    image: StaticImageData;
+    alt: string;
+  }
+
+  const images: ImageInfo[] = [
+    { image: contentImage1, alt: "image 1" },
+    { image: contentImage2, alt: "image 2" },
+    { image: contentImage3, alt: "image 3" },
+  ];
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 items-center">
+    <div className="grid grid-cols-1 md:grid-cols-2 items-center mt-24">
       <div className="content-2-wrapper">
         <h3 className="text-orange font-base mb-4">THE VIEW</h3>
         <h3 className="text-3xl font-normal font-jakarta mb-4">
@@ -20,15 +40,9 @@ export default function Content2() {
       </div>
       <div className="content-2-wrapper">
         <div className="grid grid-cols-1 md:grid-cols-3  gap-3 md:gap-5">
-          <div className="w-full md:w-auto">
-            <Modals image={contentImage1} alt="image 1" />
-          </div>
-          <div className="w-full md:w-auto">
-            <Modals image={contentImage2} alt="image 2" />
-          </div>
-          <div className="w-full md:w-auto">
-            <Modals image={contentImage3} alt="image 3" />
-          </div>
+          {images.map((imageItem, index) => (
+            <ImageItem key={index} {...imageItem} />
+          ))}
         </div>
       </div>
     </div>
--- components/Content3/Content3.tsx
+++ components/Content3/Content3.tsx
@@ -1,75 +1,314 @@
-import Image from "next/image";
-import React from "react";
+"use client";
+import Image, { StaticImageData } from "next/image";
+import React, { useState, useEffect } from "react";
 import contentImage3 from "../../public/assets/images/westvilla.png";
 import contentImage2 from "../../public/assets/images/heroimage.png";
 import iconBed from "../../public/assets/images/iconBed.png";
 import iconRice from "../../public/assets/images/iconRice.png";
 import iconFire from "../../public/assets/images/iconFire.png";
-export default function Content3() {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 md:gap-10 mt-10 items-center">
-      <div className="content-3-wrapper">
-        <div className=" carousel">
-          <div className="carousel-item w-full">
-            <Image
-              className="custom-size"
-              src={contentImage3}
-              alt="Description"
-            />
-          </div>
-          <div className="carousel-item w-full">
-            <Image
-              className="custom-size"
-              src={contentImage2}
-              alt="Description"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="content-3-wrapper mt-5">
-        <h3 className="text-orange font-base mb-4">WEST VILLA</h3>
-        <h3 className="text-3xl font-normal font-jakarta mb-4">
-          Omah Sabin West Villa
-        </h3>
-        <p className="text-blackBlur font-jakarta leading-7 mb-4 font-light	font-xl">
-          A charming haven featuring a 10m infinity pool, sun deck, and three
-          deluxe bedrooms with Bali`s renowned... <br />
-          <span className="text-blackBlur font-normal underline">
-            read more
-          </span>
-        </p>
-        <div className="flex gap-8 py-8 items-center">
-          <Image src={iconBed} alt="Icon Bed" className="w-7" />
-          <div>
-            <h3 className="text-xl font-normal font-jakarta">3 Bedrooms</h3>
-            <p className="text-blackBlur font-light font-satoshi">
-              Ideal for a group of 6 adults
-            </p>
-          </div>
-        </div>
-        <div className="flex gap-8 py-8 items-center">
-          <Image src={iconRice} alt="Icon Bed" className="w-7" />
-          <div>
-            <h3 className="text-xl font-normal font-jakarta">
-              Rice Field View Pool
-            </h3>
-            <p className="text-blackBlur font-light font-satoshi">
-              Private pool and rice field as your backyard{" "}
-            </p>
-          </div>
-        </div>
-        <div className="flex gap-8 py-8 items-center">
-          <Image src={iconFire} alt="Icon Bed" className="w-7" />
-          <div>
-            <h3 className="text-xl font-normal font-jakarta">
-              Complete Kitchen & Dining Setup
-            </h3>
-            <p className="text-blackBlur font-light font-satoshi">
-              Perfect for small gathering with your loved ones{" "}
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+import Link from "next/link";
+import { motion } from "framer-motion";
+import { useSpring, animated } from "react-spring";
+
+interface Icon {
+  src: StaticImageData;
+  alt: string;
+  title: string;
+  description: string;
 }
+
+interface ImageInfo {
+  url: StaticImageData;
+  text: string;
+}
+
+interface VillaData {
+  title: string;
+  name: string;
+  desc: string;
+  icons: Icon[];
+  images: ImageInfo[];
+}
+
+const Content3: React.FC = () => {
+  const data: VillaData[] = [
+    {
+      title: "WEST VILLA",
+      name: "Omah Sabin West Villa",
+      desc: "A charming haven featuring a 10m infinity pool, sun deck, and three deluxe bedrooms with Bali's renowned...",
+      icons: [
+        {
+          src: iconBed,
+          alt: "Icon Bed",
+          title: "3 Bedrooms",
+          description: "Ideal for a group of 6 adults",
+        },
+        {
+          src: iconRice,
+          alt: "Icon Bed",
+          title: "Rice Field View Pool",
+          description: "Private pool and rice field as your backyard",
+        },
+        {
+          src: iconFire,
+          alt: "Icon Bed",
+          title: "Complete Kitchen & Dining Setup",
+          description: "Perfect for a small gathering with your loved ones",
+        },
+      ],
+      images: [
+        {
+          url: contentImage3,
+          text: "Omah Sabin East Villa",
+        },
+        {
+          url: contentImage2,
+          text: "Omah Sabin West Villa",
+        },
+      ],
+    },
+    {
+      title: "ANOTHER VILLA",
+      name: "Another Villa Name",
+      desc: "Another description...",
+      icons: [
+        {
+          src: iconBed,
+          alt: "Icon Bed",
+          title: "Another Bedrooms",
+          description: "Ideal for another group",
+        },
+        {
+          src: iconRice,
+          alt: "Icon Bed",
+          title: "Another Rice Field View Pool",
+          description: "Private pool and rice field as another backyard",
+        },
+        {
+          src: iconFire,
+          alt: "Icon Bed",
+          title: "Another Kitchen & Dining Setup",
+          description:
+            "Perfect for another small gathering with your loved ones",
+        },
+      ],
+      images: [
+        {
+          url: contentImage3,
+          text: "Omah Sabin timur Villa",
+        },
+        {
+          url: contentImage2,
+          text: "Omah Sabin barat Villa",
+        },
+      ],
+    },
+    {
+      title: "ANOTHER VILLA 2",
+      name: "Another Villa Name 2",
+      desc: "Another description...",
+      icons: [
+        {
+          src: iconBed,
+          alt: "Icon Bed",
+          title: "Another Bedrooms",
+          description: "Ideal for another group",
+        },
+        {
+          src: iconRice,
+          alt: "Icon Bed",
+          title: "Another Rice Field View Pool",
+          description: "Private pool and rice field as another backyard",
+        },
+        {
+          src: iconFire,
+          alt: "Icon Bed",
+          title: "Another Kitchen & Dining Setup",
+          description:
+            "Perfect for another small gathering with your loved ones",
+        },
+      ],
+      images: [
+        {
+          url: contentImage3,
+          text: "Omah Sabin East Villa",
+        },
+        {
+          url: contentImage2,
+          text: "Omah Sabin West Villa",
+        },
+      ],
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [bigBannerIndex, setBigBannerIndex] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  console.log("isActive", isActive);
+
+  const prevBigSlide = () => {
+    const isFirstSlide = bigBannerIndex === 0;
+    const newIndex = isFirstSlide ? data.length - 1 : bigBannerIndex - 1;
+    setBigBannerIndex(newIndex);
+    setIsActive(true);
+    setTimeout(() => {
+      setIsActive(false);
+    }, 150);
+  };
+
+  const nextBigSlide = () => {
+    const isLastSlide = bigBannerIndex === data.length - 1;
+    const nextIndex = isLastSlide ? 0 : bigBannerIndex + 1;
+    setBigBannerIndex(nextIndex);
+    setIsActive(true);
+    setTimeout(() => {
+      setIsActive(false);
+    }, 150);
+  };
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide
+      ? data[bigBannerIndex].images.length - 1
+      : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === data[bigBannerIndex].images.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex: React.SetStateAction<number>) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [currentIndex]);
+
+  return (
+    <>
+      <div className="flex items-center justify-between max-h-fit	 mt-24">
+        <div className="flex gap-3">
+          <div className="w-6 h-6 bg-black/50 text-blackBlur cursor-pointer border border-blackBlur rounded-full flex items-center justify-center">
+            <span onClick={prevBigSlide}>{"<-"}</span>
+          </div>
+          <span
+            onClick={prevBigSlide}
+            className="font-satoshi cursor-pointer text-base font-light"
+          >
+            {data[(bigBannerIndex - 1 + data.length) % data.length].name}
+          </span>
+        </div>
+        <div className="flex items-center gap-3">
+          <span
+            onClick={nextBigSlide}
+            className="text-base font-satoshi font-light cursor-pointer "
+          >
+            {data[(bigBannerIndex + 1) % data.length].name}
+          </span>
+          <div className="w-6 h-6 bg-black/50 text-blackBlur cursor-pointer border border-blackBlur rounded-full flex items-center justify-center">
+            <span onClick={nextBigSlide}>{"->"}</span>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-10 mt-10  items-center">
+        <div className="content-3-wrapper md:col-span-1">
+          {!isActive && (
+            <motion.div
+              initial={{ x: !isActive ? -30 : 0, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1 }}
+            >
+              <div className="flex-1 max-w-[1400px] h-[566px] w-full m-auto py-16 px-4 relative group ">
+                {data[bigBannerIndex].images.map((slide, index) => (
+                  <Image
+                    key={index}
+                    src={slide.url}
+                    alt="slide-banner"
+                    fill
+                    className={`absolute w-full h-full bg-center bg-cover transition-opacity duration-1000 object-cover ${
+                      index === currentIndex ? "opacity-100" : "opacity-0"
+                    }`}
+                  />
+                ))}
+                {data[bigBannerIndex].images.map((slide, index) => (
+                  <div
+                    key={index}
+                    className={`absolute bottom-4 w-full max-w-screen-md flex justify-between items-center pr-7 ${
+                      index === currentIndex ? "opacity-100" : "opacity-0"
+                    }`}
+                  >
+                    <div className="text-white">{slide.text}</div>
+                    <div className="flex items-center gap-3">
+                      <div className="w-10 h-10 bg-black text-white cursor-pointer border border-white rounded-full flex items-center justify-center">
+                        <span onClick={prevSlide}>{"<-"}</span>
+                      </div>
+                      <div className="w-10 h-10 bg-black/50 text-white cursor-pointer border border-white rounded-full flex items-center justify-center">
+                        <span onClick={nextSlide}>{"->"}</span>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </motion.div>
+          )}
+        </div>
+        <div className="content-3-wrapper mt-5 ">
+          <motion.div
+            initial={{ x: -10, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <h3 className="text-orange font-base mb-4">
+              {data[bigBannerIndex].title}
+            </h3>
+            <h3 className="text-3xl font-normal font-jakarta mb-4">
+              {data[bigBannerIndex].name}
+            </h3>
+
+            <p className="text-blackBlur text-base font-satoshi leading-6 mb-3 md:text-xl font-light	">
+              {data[bigBannerIndex].desc} <br />
+            </p>
+            <Link
+              href="/"
+              className="text-blackBlur text-base font-medium md:text-xl underline"
+            >
+              read more
+            </Link>
+          </motion.div>
+
+          <div className={`content-3-wrapper mt-5`}>
+            {data[bigBannerIndex].icons.map((feature, index) => (
+              <motion.div
+                className="flex gap-8 py-8 items-center"
+                initial={{ x: -10, opacity: 0 }}
+                animate={{ x: 0, opacity: index === 0 ? 1 : 0.7 }}
+                transition={{ duration: 1 }}
+                key={index} // Adding a key prop is a good practice when using map in React
+              >
+                <Image src={feature.src} alt={feature.alt} className="w-7" />
+                <div>
+                  <h3 className="text-xl font-normal font-jakarta">
+                    {feature.title}
+                  </h3>
+                  <p className="text-blackBlur font-light font-satoshi">
+                    {feature.description}
+                  </p>
+                </div>
+              </motion.div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Content3;
